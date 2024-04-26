f2f7ae012ddc87e8f26dee11bde95bf37f06ac82
cosmicCode42 <cosmicanswer@gmail.com>
2024-04-26 21:49:29 +0000

    added Read functionality and added comments to models.py

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
+    # schema for the Item database
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
+    # this will show the item's name instead of 'Item Object (x)'
     def __str__(self):
         return self.name
