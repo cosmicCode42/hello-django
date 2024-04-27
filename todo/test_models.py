71eeac9869ef3913bc1c652ab0478b18642bbc30
cosmicCode42 <cosmicanswer@gmail.com>
2024-04-27 17:58:33 +0000

    added test_models

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from .models import Item
 
 
-class TestDjango(TestCase):
+class TestModels(TestCase):
     
-    def test_this_thing_works1(self):
-        self.assertEqual(1, 1)
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item') # this creates a generic item for testing
+        self.assertFalse(item.done)
+        
